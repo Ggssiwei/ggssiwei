99be2cd1f1a80604daa4eadab8b3d724ebaa8726
Gesong <382240249@qq.com>
2017-09-19 10:54:46 +0800

    1

---
+++ demo1/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>父组件在这呢!</h1>
+
+    <components1 :message="msg"></components1>
+    <input type="text" v-model="msg">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      msg: "Hello!"
+    }
+  },
+  components: {
+      components1
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo1/src/components/component1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="cp1">
+    <h2>这里是子组件</h2>
+
+    <div>
+      父组件传来的值为：{{message}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  props:['message']
+}
+</script>
+
+<style>
+#cp1 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
